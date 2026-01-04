377c718d4fafe683647b526a30eb13b991073569
Bhanu Nimmala <nimmalabhanusvc@gmail.com>
2026-01-04 16:07:50 +0530

    changed Seller Home Page UI and cretaed necessary changes like name from session and so on.

--- Satya Shop/singleshoponlineplatformproject/sellerapp/urls.py
+++ Satya Shop/singleshoponlineplatformproject/sellerapp/urls.py
@@ -6,7 +6,12 @@ from . import views
 urlpatterns = [
         path('sellerloginpage',views.sellerloginpage,name="sellerloginpage"),
         path('sellerlogin',views.sellerlogin,name="sellerlogin"),
+        path('sellerlogout',views.sellerlogout,name="sellerlogout"),
         path('sellerhome',views.sellerhomepage,name="sellerhome"),
+        path("additems", views.additemspage, name="additems"),
+        path("listitems", views.listitemspage, name="listitems"),
+        path("edititems", views.edititemspage, name="edititems"),
+        path("orders", views.sellerorderspage, name="sellerorders"),
 
 ]
 
--- Satya Shop/singleshoponlineplatformproject/sellerapp/views.py
+++ Satya Shop/singleshoponlineplatformproject/sellerapp/views.py
@@ -18,7 +18,9 @@ def sellerlogin(request):
 
             if seller.password == pwd:
                 request.session["seller_id"] = seller.id
-                request.session["seller_name"] = seller.shop_name
+                request.session["seller_shop_name"] = seller.shop_name
+                request.session["seller_name"] = seller.seller_name
+                #print(seller.seller_name)
                 return redirect("sellerhome")
             else:
                 messages.error(request, "Invalid password")
@@ -28,7 +30,37 @@ def sellerlogin(request):
 
     return redirect("sellerloginpage")
 
+def sellerlogout(request):
+    request.session.flush()
+    return redirect("sellerloginpage")
+
 def sellerhomepage(request):
     if "seller_id" not in request.session:
         return redirect("sellerloginpage")
     return render(request, "seller/sellerhome.html")
+
+def additemspage(request):
+    if "seller_id" not in request.session:
+        return redirect("sellerloginpage")
+    return render(request, "seller/additems.html")
+
+
+# List Items page
+def listitemspage(request):
+    if "seller_id" not in request.session:
+        return redirect("sellerloginpage")
+    return render(request, "seller/listitems.html")
+
+
+# Edit Items page
+def edititemspage(request):
+    if "seller_id" not in request.session:
+        return redirect("sellerloginpage")
+    return render(request, "seller/edititems.html")
+
+
+# Seller Orders page
+def sellerorderspage(request):
+    if "seller_id" not in request.session:
+        return redirect("sellerloginpage")
+    return render(request, "seller/sellerorders.html")

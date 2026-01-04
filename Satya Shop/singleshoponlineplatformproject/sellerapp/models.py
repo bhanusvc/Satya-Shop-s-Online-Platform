772bebd27014976562b53846ee858a3719b50ce5
Bhanu Nimmala <nimmalabhanusvc@gmail.com>
2026-01-04 12:06:30 +0530

    Created Seller Model(added to Admin.py), Superuser(added 1 seller data via Admin Dashboard)

--- Satya Shop/singleshoponlineplatformproject/sellerapp/models.py
+++ Satya Shop/singleshoponlineplatformproject/sellerapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Seller(models.Model):
+    shop_name = models.CharField(max_length=50, blank=False)
+    seller_name = models.CharField(max_length=40,blank=False)
+    email = models.EmailField(max_length=100, blank=False, unique=True)
+    username = models.CharField(max_length=100, blank=False, unique=True)
+    password = models.CharField(max_length=100, blank=False)
+    contact = models.BigIntegerField(blank=False)
+    location = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return f"{self.shop_name} ({self.seller_name})"
+    class Meta:
+        db_table = "seller_table"
